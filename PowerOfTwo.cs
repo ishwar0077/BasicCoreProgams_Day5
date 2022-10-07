ad596d4e3ad0bdf58db891b55ddff8d1bb3eef66
Ishwar Rathod <ishwarkrathod.o7@gmail.com>
2022-10-07 19:39:12 +0530

    Calculate Power of two

---
+++ PowerOfTwo.cs
@@ -0,0 +1,48 @@
+﻿/*
+ 3. Power of 2
+a. Desc -> This program takes a command-line argument N and prints a table of the
+powers of 2 that are less than or equal to 2^N.
+b. I/P -> The Power Value N. Only works if 0 <= N < 31 since 2^31 overflows an int
+c. Logic -> repeat until i equals N.
+d. O/P -> Print the year is a Leap Year or not.
+/*
+3. Power of 2
+a. Desc -> This program takes a command-line argument N and prints a table of the
+powers of 2 that are less than or equal to 2^N.
+b. I/P -> The Power Value N. Only works if 0 <= N < 31 since 2^31 overflows an int
+c. Logic -> repeat until i equals N.
+d. O/P -> Print the year is a Leap Year or not.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BasicCoreProgam
+{
+    public static class PowerOfTwo
+    {
+		public static void DisplayPower()
+		{
+			int num;
+			int mul = 1;
+
+			Console.WriteLine("Enter the number");
+			num = Convert.ToInt32(Console.ReadLine());
+			if (num >= 0 && num > 31) 
+			{
+				Console.WriteLine("value of number should be 0 to 30");
+				
+			}
+
+			else 
+			{
+				Console.WriteLine(mul);
+				for (int i = 1; i <= num; i++) {
+					mul = mul * 2;
+                    Console.WriteLine(mul);
+				}
+			}
+		}
+    }
+}
