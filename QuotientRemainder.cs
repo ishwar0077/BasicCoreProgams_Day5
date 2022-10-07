ba955d5f255f174917ea344949e6656fe1606dd7
Ishwar Rathod <ishwarkrathod.o7@gmail.com>
2022-10-07 19:53:56 +0530

    Calculete Quotient and Remainder

---
+++ QuotientRemainder.cs
@@ -0,0 +1,23 @@
+﻿// C# Program to Compute Quotient and Remainder
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public static class QuotientRemainder
+    {
+        public static void CheckQuotientRemainder()
+        {
+            int divident = 24, divider = 2;
+            int quotient = divident / divider;
+            int remainder = divident % divider;
+            Console.WriteLine("divident: {0}, divider: {1}", divident, divider);
+            Console.WriteLine("Quotient: " + quotient);
+            Console.WriteLine("Remainder: " + remainder);
+
+        }
+    }
+}
