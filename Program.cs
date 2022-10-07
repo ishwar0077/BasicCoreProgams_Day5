dc4510e164f9b8a1a52d4080b23000da4e4762b1
Ishwar Rathod <ishwarkrathod.o7@gmail.com>
2022-10-07 19:24:24 +0530

    Basic Core Program

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using BasicCoreProgram;
+namespace BasicCoreProgam
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            FlipCoin.CalFlipCoin();
+        }
+    }
+}
