7d71ae92a7277bc5e74cc6d010c9f2703046895a
Ishwar Rathod <ishwarkrathod.o7@gmail.com>
2022-10-07 19:56:22 +0530

    Swaping Two Number

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace BasicCoreProgam
     {
         public static void Main(string[] args)
         {
-            QuotientRemainder.CheckQuotientRemainder();
+            SwapTwoNumber.Swap();
 
         }
     }
---
+++ SwapTwoNumber.cs
@@ -0,0 +1,23 @@
+﻿// 7. C# Program to Swap Two Numbers
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public static class SwapTwoNumber
+    {
+        public static void Swap()
+        {
+            int a = 10, b = 5;
+            Console.WriteLine("Numbers before swap a = {0} and b = {1}", a, b);
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.WriteLine("Numbers after swap a = {0} and b = {1}", a, b);
+
+        }
+    }
+}
